d7d72c0e4e7879b170a7532fd69c12fc115fc26f
Mohit BAGHEL <baghelmohit66@gmail.com>
2020-04-28 15:14:41 +0530

    auto slider default version

--- app/src/main/java/com/example/autoslider/MainActivity.java
+++ app/src/main/java/com/example/autoslider/MainActivity.java
@@ -5,59 +5,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.smarteist.autoimageslider.DefaultSliderView;
 import com.smarteist.autoimageslider.IndicatorAnimations;
 import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderLayout;
 import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    SliderView sliderView;
-    private SliderAdapterExample adapter;
+    SliderLayout sliderLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sliderView = findViewById(R.id.imageSlider);
+        sliderLayout = findViewById(R.id.imageSlider);
+        sliderLayout.setIndicatorAnimation(IndicatorAnimations.FILL); //set indicator animation by using SliderLayout.Animations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderLayout.setScrollTimeInSec(1); //set scroll delay in seconds :
 
-        adapter = new SliderAdapterExample(this);
-        sliderView.setSliderAdapter(adapter);
-
-        sliderView.setIndicatorAnimation(IndicatorAnimations.THIN_WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
-        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
-        sliderView.setIndicatorSelectedColor(Color.WHITE);
-        sliderView.setIndicatorUnselectedColor(Color.GRAY);
-        sliderView.setScrollTimeInSec(3);
-        sliderView.setAutoCycle(false);
+        setSliderViews();
     }
-        public void renewItems(View view) {
-            List<SliderItem> sliderItemList = new ArrayList<>();
-            //dummy data
-            for (int i = 0; i < 5; i++) {
-                SliderItem sliderItem = new SliderItem();
-                sliderItem.setDescription("Slider Item " + i);
-                if (i % 2 == 0) {
-                    sliderItem.setImageUrl("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
-                } else {
-                    sliderItem.setImageUrl("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260");
-                }
-                sliderItemList.add(sliderItem);
+
+    private void setSliderViews() {
+
+        for (int i = 0; i <= 3; i++) {
+
+            DefaultSliderView sliderView = new DefaultSliderView(this);
+
+            switch (i) {
+                case 0:
+                    sliderView.setImageDrawable(R.mipmap.puma);
+//                    sliderView.setImageUrl("https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+                    break;
+                case 1:
+                    sliderView.setImageDrawable(R.mipmap.puma);
+//                    sliderView.setImageUrl("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+                    break;
+                case 2:
+                    sliderView.setImageDrawable(R.mipmap.puma);
+//                    sliderView.setImageUrl("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260");
+                    break;
+                case 3:
+                    sliderView.setImageDrawable(R.mipmap.puma);
+//                    sliderView.setImageUrl("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+                    break;
             }
-            adapter.renewItems(sliderItemList);
-        }
 
-        public void removeLastItem(View view) {
-            if (adapter.getCount() - 1 >= 0)
-                adapter.deleteItem(adapter.getCount() - 1);
-        }
+            sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP);
+//            sliderView.setDescription("setDescription " + (i + 1));
+            final int finalI = i;
+            sliderView.setOnSliderClickListener(new SliderView.OnSliderClickListener() {
+                @Override
+                public void onSliderClick(SliderView sliderView) {
+                    Toast.makeText(MainActivity.this, "This is slider " + (finalI + 1), Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        public void addNewItem(View view) {
-            SliderItem sliderItem = new SliderItem();
-            sliderItem.setDescription("Slider Item Added Manually");
-            sliderItem.setImageUrl("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
-            adapter.addItem(sliderItem);
+            //at last add this view in your layout :
+            sliderLayout.addSliderView(sliderView);
         }
     }
+}
